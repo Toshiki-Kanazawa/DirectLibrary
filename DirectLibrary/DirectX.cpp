866c527cb8eca7bf21a80f039d15c0e02416e8b1
naonao7700 <104463154+naonao7700@users.noreply.github.com>
2022-12-12 16:05:44 +0900

    画面を青色に塗りつぶす

--- DirectLibrary/DirectX.cpp
+++ DirectLibrary/DirectX.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "DirectX.h"
 #include <Windows.h>
 
@@ -36,6 +37,17 @@ void SetupDxgiSwapChainDesc(DXGI_SWAP_CHAIN_DESC* dxgi)
 //デバイスとスワップチェインとコンテキストを作成する
 void CreateDeviceAndSwapChain( IDXGISwapChain* swapChain, ID3D11Device* device, ID3D11DeviceContext* context )
 {
+}
+
+//レンダーターゲットを作成する
+void CreateRenderTarget()
+{
+
+}
+
+bool DirectX::Init()
+{
+	//デバイスとコンテキストとスワップチェインの作成
 	DXGI_SWAP_CHAIN_DESC dxgi;
 	SetupDxgiSwapChainDesc(&dxgi);
 	D3D_FEATURE_LEVEL level;
@@ -55,16 +67,48 @@ void CreateDeviceAndSwapChain( IDXGISwapChain* swapChain, ID3D11Device* device, 
 		&level,
 		&context
 	);
-}
-
-bool DirectX::Init()
-{
-	//デバイスとコンテキストとスワップチェインの作成
-	CreateDeviceAndSwapChain( swapChain, device, context);
 
 	//TODO::レンダーターゲットの作成
 
+	ID3D11Texture2D* back_buffer;
+	swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&back_buffer);
+	device->CreateRenderTargetView(back_buffer, nullptr, &renderTargetView);
+
+	back_buffer->Release();
+
+	//デプスステンシルの作成
+	HWND window_handle = FindWindow(L"DirectLibrary", nullptr);
+	RECT rect;
+	GetClientRect(window_handle, &rect);
+
+	D3D11_TEXTURE2D_DESC texture_desc;
+	ZeroMemory(&texture_desc, sizeof(D3D11_TEXTURE2D_DESC));
+	texture_desc.Width = (rect.right) - (rect.left);
+	texture_desc.Height = (rect.bottom) - (rect.top);
+	texture_desc.MipLevels = 1;
+	texture_desc.ArraySize = 1;
+	texture_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	texture_desc.SampleDesc.Count = 1;
+	texture_desc.SampleDesc.Quality = 0;
+	texture_desc.Usage = D3D11_USAGE_DEFAULT;
+	texture_desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	texture_desc.CPUAccessFlags = 0;
+	texture_desc.MiscFlags = 0;
+
+	device->CreateTexture2D(&texture_desc, NULL, &texture);
+
+	D3D11_DEPTH_STENCIL_VIEW_DESC dsv_desc;
+	ZeroMemory(&dsv_desc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+	dsv_desc.Format = texture_desc.Format;
+	dsv_desc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	dsv_desc.Texture2D.MipSlice = 0;
+
+	device->CreateDepthStencilView(texture, &dsv_desc, &depthStencilView);
+
+
+
 	//TODO::ビューポートの設定
+
 
 	//TODO::シェーダーの作成
 
@@ -80,13 +124,15 @@ void DirectX::Release()
 //描画開始関数
 void DirectX::StartRendering()
 {
-
+	float clear_color[4] = { 0.0f, 0.0f, 1.0f, 1.0f };
+	context->ClearRenderTargetView(renderTargetView, clear_color);
+	context->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 }
 
 //描画終了関数
 void DirectX::FinishRendering()
 {
-
+	swapChain->Present(0, 0);
 }
 
 //ポリゴンを描画する関数
