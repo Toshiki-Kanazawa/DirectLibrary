ac18ba56427286ca5bcdadb95bf093285ba1e9e3
Toshiki-Kanazawa <109838634+Toshiki-Kanazawa@users.noreply.github.com>
2023-01-24 16:18:30 +0900

    Player ジャンプ修正

--- DirectLibrary/GameApp/Player/Player.cpp
+++ DirectLibrary/GameApp/Player/Player.cpp
@@ -13,24 +13,27 @@ Player::~Player()
 
 void Player::Start()
 {
-	player->posX = 100;
-	player->posY = 100;
-	player->posZ = 100;
-	player->rotX = 100;
-	player->rotY = 100;
-	player->rotZ = 100;
-	player->scaleX = 1;
-	player->scaleY = 1;
-	player->scaleZ = 1;
+	player->posX = 0.0f;
+	player->posY = 0.0f;
+	player->posZ = 0.0f;
+	player->rotX = 0.0f;
+	player->rotY = 0.0f;
+	player->rotZ = 0.0f;
+	player->scaleX = 1.0f;
+	player->scaleY = 1.0f;
+	player->scaleZ = 1.0f;
 }
 
 void Player::Update()
 {
-	player->posY -= gravity; // プレイヤーに常に重力を加える
+	vy = 0.0f;
 
-	if (player->posY > 0)    // 地上にいるときでしかジャンプできない
+	if (player->posY > 0)    // ジャンプ中
 	{
-		jump_flag = false;
+		jump_flag = false;  // ジャンプできない
+
+		vy += gravity;      // 移動量に重力を加える
+		player->posY -= vy; // プレイヤーに重力を加える
 	}
 
 	if (InputManager::On(KeyType::Right)) // 右
@@ -49,7 +52,7 @@ void Player::Update()
 	{
 		player->posZ -= 0.01f;
 	}
-	if (InputManager::Trg(KeyType::Space)) // ジャンプ
+	if (InputManager::On(KeyType::Space)) // ジャンプ
 	{
 		if (!jump_flag)
 		{
@@ -85,6 +88,7 @@ void Player::SetPositon(Vector3 vec)
 void Player::Jump()
 {
 	jump_flag = true;
+
 	if (player->posY < jump_hight)
 	{
 		player->posY += jump_speed;
--- DirectLibrary/GameApp/Player/Player.h
+++ DirectLibrary/GameApp/Player/Player.h
@@ -8,11 +8,11 @@ private:
 	float sizeX = 10.0f;
 	float sizeY = 10.0f;
 	float sizeZ = 10.0f;
-	bool jump_flag = false; // ジャンプ中フラグ
-	float jump_hight = 10.0f; // ジャンプ高さ
-	float jump_speed = 0.05f;
-	const float gravity = 0.02f; // 重力
-
+	bool jump_flag = false;         // ジャンプフラグ(true→できる、false→できない)
+	const float jump_hight = 30.0f; // ジャンプ高さ
+	const float jump_speed = 0.05f; // ジャンプ速度
+	const float gravity = 0.03f;    // 重力
+	float vy = 0.0f;                // 移動量
 	void Jump();
 public:
 	Player();
