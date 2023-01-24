8ac1e75194fc6cac570dd8ca52ea7cfb02f043c9
Toshiki-Kanazawa <109838634+Toshiki-Kanazawa@users.noreply.github.com>
2023-01-24 15:06:25 +0900

    Player 移動、GetPosition関数、GetSize関数、SetPositon関数

--- DirectLibrary/GameApp/Player/Player.h
+++ DirectLibrary/GameApp/Player/Player.h
@@ -5,10 +5,17 @@ class Player
 {
 private:
 	ModelObject* player = nullptr;
+	float sizeX = 10;
+	float sizeY = 10;
+	float sizeZ = 10;
 public:
 	Player();
 	~Player();
 	void Start();
 	void Update();
 	void Render();
+	Vector3 GetPosition(); // pos‚ğ•Ô‚·
+	Vector3 GetSize();     // size‚ğ•Ô‚·
+	void SetPositon(Vector3 vec);  // pos‚ğ•ÏX‚·‚é
+
 };
