32c17750aa30418b2f5a1044ce2f7974128c7cd9
itsukiM <i48morita@gmail.com>
2023-01-24 15:12:46 +0900

    GetPlayerNextPos追加(ステージ)

--- DirectLibrary/GameApp/Stage/Stage.cpp
+++ DirectLibrary/GameApp/Stage/Stage.cpp
@@ -2,10 +2,39 @@
 
 Stage::Stage() {
 	Floor = new ModelObject( L"Floor.txt" );
+
+	WorldSize_min = Vector3(-100, -100, -100);
+	WorldSize_max = Vector3(100, 100, 100);
 }
 
 Stage::~Stage() {
 	delete Floor;
+}
+
+Vector3 Stage::GetPlayerNextPos(Vector3 pl_pos, Vector3 pl_size)
+{
+	Vector3 resultPos = pl_pos;
+
+	if (pl_pos.x - pl_size.x < WorldSize_min.x) {
+		resultPos.x = WorldSize_min.x + pl_size.x;
+	}
+	if (pl_pos.x - pl_size.x > WorldSize_max.x) {
+		resultPos.x = WorldSize_max.x - pl_size.x;
+	}
+	if (pl_pos.y - pl_size.y < WorldSize_min.y) {
+		resultPos.y = WorldSize_min.y + pl_size.y;
+	}
+	if (pl_pos.y - pl_size.y > WorldSize_max.y) {
+		resultPos.y = WorldSize_max.y - pl_size.y;
+	}
+	if (pl_pos.z - pl_size.z < WorldSize_min.z) {
+		resultPos.z = WorldSize_min.z + pl_size.z;
+	}
+	if (pl_pos.z - pl_size.z > WorldSize_max.z) {
+		resultPos.z = WorldSize_max.z - pl_size.z;
+	}
+
+	return resultPos;
 }
 
 void Stage::Start() {
--- DirectLibrary/GameApp/Stage/Stage.h
+++ DirectLibrary/GameApp/Stage/Stage.h
@@ -5,9 +5,14 @@
 class Stage{
 private:
 	ModelObject* Floor;
+
+	Vector3 WorldSize_min;
+	Vector3 WorldSize_max;
 public:
 	Stage();
 	~Stage();
+
+	Vector3 GetPlayerNextPos(Vector3 pl_pos, Vector3 pl_size);
 
 	void Start();
 	void Update();
