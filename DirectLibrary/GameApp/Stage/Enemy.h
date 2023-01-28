68475de884e353cd769c2855b09bffea70a983f6
itsukiM <i48morita@gmail.com>
2023-01-28 10:56:11 +0900

    Floor位置・Enemyサイズ修正

--- DirectLibrary/GameApp/Stage/Enemy.h
+++ DirectLibrary/GameApp/Stage/Enemy.h
@@ -4,9 +4,9 @@
 class Enemy{
 private:
 	ModelObject* enemy;
-	float sizeX = 10;
-	float sizeY = 10;
-	float sizeZ = 10;
+	float sizeX = 0.5;
+	float sizeY = 0.5;
+	float sizeZ = 0.5;
 
 	Vector3 pos;// = Vector3(enemy->posX, enemy->posY, enemy->posZ);
 	Vector3 moveDirection;
