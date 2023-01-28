e51882effdadaae47e1fb8211b55b57bf9864ff7
Toshiki-Kanazawa <109838634+Toshiki-Kanazawa@users.noreply.github.com>
2023-01-28 11:18:19 +0900

    EnemyManager 出現位置変更、敵スピード変更

--- DirectLibrary/GameApp/EnemyManager/EnemyManager.cpp
+++ DirectLibrary/GameApp/EnemyManager/EnemyManager.cpp
@@ -43,33 +43,34 @@ void EnemyManager::EnemyPop()
 	Vector3 dir; // 方向
 
 	auto num = rand() % 4; // 敵の出現方向の乱数
+	auto rnd = rand() % 20 - 10;
 
 	if (num == 0)
 	{
-		pos = Vector3(0,0,-10);
+		pos = Vector3(rnd,0,-10);
 		dir = Vector3(0,0,1);   // 下から出現
 		popCount++;
 	}
 	if (num == 1)
 	{
-		pos = Vector3(0,0,10);
+		pos = Vector3(rnd,0,10);
 		dir = Vector3(0,0,-1);  // 上から出現
 		popCount++;
 	}
 	if (num == 2)
 	{
-		pos = Vector3(10,0,0);
+		pos = Vector3(10,0,rnd);
 		dir = Vector3(-1,0,0);  // 右から出現
 		popCount++;
 	}
 	if (num == 3)
 	{
-		pos = Vector3(-10,0,0);
+		pos = Vector3(-10,0,rnd);
 		dir = Vector3(1,0,0);   // 左から出現
 		popCount++;
 	}
 
-	auto spd = 0.001f + (popCount / 10) * 1.0f;
+	auto spd = 0.001f + (popCount / 10) * 0.001f;
 	auto createFlag = false;	//敵キャラを生成したフラグをOFF
 	for (int i = 0; i < enemies.size(); ++i)
 	{
